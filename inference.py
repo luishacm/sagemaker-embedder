7e22aa9333ccb0b07d64ae917110ee8e94cd715e
luishacm <38386755+luishacm@users.noreply.github.com>
2023-03-29 15:18:44 -0300

    first commit

---
+++ inference.py
@@ -0,0 +1,48 @@
+from transformers import AutoTokenizer, AutoModel
+import torch
+import torch.nn.functional as F
+
+# Helper: Mean Pooling - Take attention mask into account for correct averaging
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
+def model_fn(model_dir):
+    # Load model from HuggingFace Hub
+    tokenizer = AutoTokenizer.from_pretrained(model_dir)
+    model = AutoModel.from_pretrained(model_dir)
+
+    # Move model to GPU if available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    return model, tokenizer
+
+def predict_fn(data, model_and_tokenizer):
+    # destruct model and tokenizer
+    model, tokenizer = model_and_tokenizer
+
+    # Tokenize sentences
+    sentences = data.pop("inputs", data)
+    encoded_input = tokenizer(sentences, padding=True, truncation=True, return_tensors='pt', max_length=256)
+    # Move input tensors to GPU if available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    for key, value in encoded_input.items():
+        encoded_input[key] = value.to(device)
+
+    # Compute token embeddings
+    with torch.no_grad():
+        model_output = model(**encoded_input)
+
+    # Perform pooling
+    sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
+
+    # Normalize embeddings
+    sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=1)
+
+    # Move embeddings back to CPU to return
+    sentence_embeddings = sentence_embeddings.cpu()
+
+    # Return dictionary, which will be JSON serializable
+    return {"vectors": sentence_embeddings[0].tolist()}
