b933314d585c4639f190138972a3f6c4bb88140a
luishacm <38386755+luishacm@users.noreply.github.com>
2024-03-27 08:13:27 -0300

    [REFACTOR] reorganizing code

--- server_inference.py
+++ server_inference.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, AutoModel
 import torch
 import torch.nn.functional as F
 
-# Helper: Mean Pooling - Take attention mask into account for correct averaging
+#Take attention mask into account for correct averaging
 def mean_pooling(model_output, attention_mask):
     token_embeddings = model_output[0] #First element of model_output contains all token embeddings
     input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
